ec624831b51e0051d8050a51b219cccf73f1d847
Yura Khomenko <yurakhomenko2017@gmail.com>
2020-03-30 21:57:23 +0300

    Revert "Закоментував строчки в homeWork1.js"
    This reverts commit 79f3af02dcd73d23684d4486c3a049bc7e8b18bf.

--- homeWork1.js
+++ homeWork1.js
@@ -2,24 +2,24 @@
 
  if (memory == 64) {
     alert ("Phone price 300$");
-  /*  document.write('<img src="iphoneyellow.jpg" alt="" width="500px" height="700px"> ');
+    document.write('<img src="iphoneyellow.jpg" alt="" width="500px" height="700px"> ');
     document.write("<br> <br> <br> <br>");
     document.write('<img src="iphonegreen.jpg" alt="" width="500px" height="700px"> ');
     document.write("<br> <br> <br> <br>");
-    document.write('<img src="iphoneblue.jpg" alt="" width="500px" height="700px">');*/
+    document.write('<img src="iphoneblue.jpg" alt="" width="500px" height="700px">');
  }
  else if (memory == 128) {
     alert ("Phone price 500$");
-  /*  document.write('<img src="iphoneblue.jpg" alt="" width="500px" height="700px">');
+    document.write('<img src="iphoneblue.jpg" alt="" width="500px" height="700px">');
     document.write("<br> <br> <br> <br>");
-    document.write('<img src="iphonegrey.jpg" alt="" width="500px" height="700px">');*/
+    document.write('<img src="iphonegrey.jpg" alt="" width="500px" height="700px">');
  }
  else if (memory == 256) {
     alert ("Phone Price 700$");
-  /*  document.write('<img src="iphoneyellow.jpg" alt="" width="500px" height="700px">');
+    document.write('<img src="iphoneyellow.jpg" alt="" width="500px" height="700px">');
     document.write("<br> <br> <br> <br>");
-    document.write('<img src="iphonegreen.jpg" alt="" width="500px" height="700px">');*/
- }
+    document.write('<img src="iphonegreen.jpg" alt="" width="500px" height="700px">');
+ } 
  else {
     alert ("Incorrect type");
  }
@@ -27,14 +27,14 @@
 
  document.write("<img src='iphoneyellow.jpg' alt='' width='1000px' height='1200px'>");
 
-
+ 
 
 /*
  let color = prompt("What color do you want?");
     if (color = yellow) {
        document.write("<img src='iphoneyellow.jpg' alt='' width='1000px' height='1200px>");
     }
-    else if (color == green) {
+    else if (color == green) { 
     document.write('<img src="iphonegreen.jpg" alt="" width="1000px" height="1200px"> ');
     }
     else if (color == blue) {
