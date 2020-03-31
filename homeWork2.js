a4a74d711651b219889c51b00a423cf652467d9c
Yura Khomenko <yurakhomenko2017@gmail.com>
2020-03-31 17:33:42 +0300

    Добавив if умову

--- homeWork2.js
+++ homeWork2.js
@@ -1,23 +1,24 @@
 let price;
 let memory = prompt("How much memory do you want in phone?");
 
+if (memory === null || !+memory) {
+    alert("Wrong");
+  }
+else {
 if (memory == 64) {
    price = "Phone price 300$";
-  
+
 }
 else if (memory == 128) {
     price = "Phone price 500$";
-  
+
 }
 else if (memory == 256) {
     price = "Phone price 700$";
-  
-} 
+
+}
 else {
    alert ("Incorrect type");
 }
+}
 alert(price);
-/*
-document.write (price);
-Можливо потрібно так?
-*/
